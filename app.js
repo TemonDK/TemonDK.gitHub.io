ab14aa6e6c5dba5702e6a9146cbf6bf613a64395
TemonDK <165452969+TemonDK@users.noreply.github.com>
2024-04-26 00:14:45 +0400

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
-
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
 btn1.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
 		tg.MainButton.hide();
@@ -79,7 +80,26 @@ btn6.addEventListener("click", function(){
 		tg.MainButton.show();
 	}
 });
-
+btn7.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 7!");
+		item = "7";
+		tg.MainButton.show();
+	}
+});
+btn8.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 8!");
+		item = "8";
+		tg.MainButton.show();
+	}
+});
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
